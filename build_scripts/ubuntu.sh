768bccf8a55f8f9161092bc18c5cd647063b56c1
Cesar Vega <cesar.binary@gmail.com>
2026-03-07 10:22:22 -0800

    Corrected build script version argument

--- build_scripts/ubuntu.sh
+++ build_scripts/ubuntu.sh
@@ -1,9 +1,15 @@
+VERSION=$1
+if [ -z "$VERSION" ]; then
+    echo "Cannot build - need version number argument, i.e.:"
+    echo "./ubuntu.sh v1.2.3"
+    exit 1
+fi
 cd ..
 mkdir tmp
 go build -o tmp/ha_network_failover main.go
 cp config.example.json tmp/
 mkdir tmp/storage
 cd tmp
-tar -czvf ../ubuntu-$0.tar.gz .
+tar -czvf ../ubuntu-${VERSION}.tar.gz .
 cd ..
 rm -rf tmp/
